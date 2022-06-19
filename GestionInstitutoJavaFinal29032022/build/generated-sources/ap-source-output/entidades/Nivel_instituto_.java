29dd5989d24a252cf7f1e505f1837bf7ea6acd82
mohammed sebti <sebti74@gmail.com>
2022-06-19 23:16:28 +0200

    mas modificaciones y correciones 19 06 2022 23 16

--- GestionInstitutoJavaFinal29032022/build/generated-sources/ap-source-output/entidades/Nivel_instituto_.java
+++ GestionInstitutoJavaFinal29032022/build/generated-sources/ap-source-output/entidades/Nivel_instituto_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-14T23:32:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-16T13:41:16")
 @StaticMetamodel(Nivel_instituto.class)
 public class Nivel_instituto_ { 
 
--- GestionInstitutoJavaFinal29032022/build/generated-sources/ap-source-output/entidades/SubidaDocumento_.java
+++ GestionInstitutoJavaFinal29032022/build/generated-sources/ap-source-output/entidades/SubidaDocumento_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-14T23:32:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-06-16T13:41:16")
 @StaticMetamodel(SubidaDocumento.class)
 public class SubidaDocumento_ { 
 
